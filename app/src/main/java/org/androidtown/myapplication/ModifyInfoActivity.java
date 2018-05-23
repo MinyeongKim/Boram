bd829a846ea43778642a1d3e9be2a933b9e868cd
SuJeong2 <sj971020@naver.com>
2018-05-23 10:25:31 +0900

    2018-05-23
    Add Modify user information

---
+++ app/src/main/java/org/androidtown/myapplication/ModifyInfoActivity.java
@@ -0,0 +1,66 @@
+package org.androidtown.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ModifyInfoActivity extends AppCompatActivity {
+
+    EditText loginID, loginPW, userName;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setupActionBar();
+
+        loginID=(EditText)findViewById(R.id.loginId);
+        loginPW=(EditText)findViewById(R.id.loginPw);
+        userName=(EditText)findViewById(R.id.userName);
+
+        submit=(Button)findViewById(R.id.modify);
+        submit.setOnClickListener(new Button.OnClickListener(){
+
+            //회원정보 수정
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    //뒤로가는 버튼 생성
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    //뒤로가기 버튼이 눌렀을 경우
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //NavUtils.navigateUpFromSameTask(this);
+                finish();
+                return true;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
