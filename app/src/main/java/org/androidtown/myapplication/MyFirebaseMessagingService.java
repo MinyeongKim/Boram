8ac9f32c10d4480763e3e342b1419c46f84ad906
SurimYuk <33217962+SurimYuk@users.noreply.github.com>
2018-06-06 03:49:31 +0900

    .

--- app/src/main/java/org/androidtown/myapplication/MyFirebaseMessagingService.java
+++ app/src/main/java/org/androidtown/myapplication/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package org.androidtown.myapplication;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,29 +24,25 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         //Map<String, String> bundle = remoteMessage.getData();
         super.onMessageReceived(remoteMessage);
-        if (remoteMessage.getNotification() != null) {
+        if (remoteMessage.getNotification() != null) { //&&id==처음받기, &&id==평가받기
             String body = remoteMessage.getNotification().getBody();
             String title = remoteMessage.getNotification().getTitle();
             Log.d(TAG, "Notification Body: " + body);
 
+            //각 다른 역할에 따라 다른 서비스 제공해야함.//requestCode???가 뭐지???
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
             NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext())
                     .setSmallIcon(R.mipmap.icon_v1) //알림 영역에 노출 될 아이콘
                     //.setContentTitle(getString(R.string.app_name)) //알림 영역에 노출 될 타이틀
                     .setContentTitle(title)
-                    .setContentText(body); //Firebase Console에서 사용자가 전달한 메시지 내용
+                    .setContentText(body) //Firebase Console에서 사용자가 전달한 메시지 내용
+                    .setContentIntent(pendingIntent);//푸시 눌렀을때 반응.
             NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
             notificationManagerCompat.notify(0x1001, notificationBuilder.build());
+            //notificationManagerCompat.cancel(id); --> 평가 완료 시에 부여되는 id를 통해서 없애야할듯.
 
-            sendToActivity();
         }
-
-    }
-
-    private void sendToActivity() {
-        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        startActivity(intent);
 
     }
 
