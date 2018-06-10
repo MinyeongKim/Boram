762cf17fd0fe787b67ea1a1b9f9a86ac0c8cc123
SurimYuk <33217962+SurimYuk@users.noreply.github.com>
2018-06-11 04:03:17 +0900

    .

--- app/src/main/java/org/androidtown/myapplication/FeedbackActivity.java
+++ app/src/main/java/org/androidtown/myapplication/FeedbackActivity.java
@@ -14,26 +14,31 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
 
 //import org.gcsw.boram.R;
 
 public class FeedbackActivity extends BaseActivity{
-
+    StorageReference spaceRef;
     TextView habitTitle, date, writeDate,comment,feedback_date,feedback_comment;
     RatingBar rate, feedback_rate;
-    ImageView imageLoad;
+    ImageView user_imageLoad;
 
     String date_value, writeDate_value,comment_value,feedback_date_value,feedback_comment_value, rate_value, feedback_rate_value;
 
     FirebaseDatabase database;
     DatabaseReference databaseReference;
+    private FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +60,14 @@ public class FeedbackActivity extends BaseActivity{
         rate = (RatingBar)findViewById(R.id.user_rate);
         feedback_rate = (RatingBar)findViewById(R.id.feedback_rate);
 
-        imageLoad = (ImageView)findViewById(R.id.imageLoad);
+        user_imageLoad = (ImageView)findViewById(R.id.user_imageLoad);
 
         Intent info = getIntent();
         Bundle get_info = info.getExtras();
 
-        String user =get_info.getString("UserID");
+        String filename = get_info.getString("filename");
+        Toast.makeText(getApplicationContext(), filename, Toast.LENGTH_SHORT).show();
+        String user = get_info.getString("UserID");
 
         final int habitIndex = get_info.getInt("HABITINDEX");
         final int index = get_info.getInt("INDEX");
@@ -77,6 +84,9 @@ public class FeedbackActivity extends BaseActivity{
         if(withWho.equals("friend")){
             String friendID = get_info.getString("FriendID");
         }
+
+        spaceRef = storage.getReferenceFromUrl("gs://mobileproject-57744.appspot.com/").child("images/" + filename);
+        Glide.with(this).using(new FirebaseImageLoader()).load(spaceRef).into(user_imageLoad);
 
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference("users/" + user + "/habits/current/" + habitIndex + "/history/"+index);
@@ -104,6 +114,8 @@ public class FeedbackActivity extends BaseActivity{
 
                 rate.setRating(Float.parseFloat(rate_value));
                 feedback_rate.setRating(Float.parseFloat(feedback_rate_value));
+
+
             }
 
             @Override
